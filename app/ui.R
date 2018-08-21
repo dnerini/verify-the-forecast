83ed14af91860d59b017308301858b50cfff8948
dnerini <daniele.nerini@gmail.com>
2018-08-21 21:31:20 +0200

    New time range

--- app/ui.R
+++ app/ui.R
@@ -14,13 +14,19 @@ ui <- fluidPage(
     sidebarPanel(
       checkboxGroupInput("forecasts", "Forecasts to evaluate:", choices=list.providers.labels,
                          inline=FALSE, width=NULL, choiceNames=NULL, selected=list.providers.labels),
-      dateRangeInput("daterange1", "Validation period",
+      
+      selectInput("range", "Time range", list.time.ranges, selected=NULL, multiple=FALSE,
+                  selectize=TRUE, size=NULL, width="36%"),
+      conditionalPanel(
+        condition = "input.range == 'select'",
+        dateRangeInput("daterange1", "Select time range",
                      start="2018-08-01", min="2018-08-01",
-                     end=Sys.Date() - 1, max=Sys.Date() - 1),
+                     end=Sys.Date() - 1, max=Sys.Date() - 1)),
       
       div(style="display: inline-block;vertical-align:top; width: 200px;",
         selectInput("region", "Region", list.region.menu, selected=NULL, multiple=FALSE,
                   selectize=TRUE, size=NULL, width="100%")),
+      
       div(style="display: inline-block;vertical-align:top; width: 20px;",HTML("<br>")),
       div(style="display: inline-block;vertical-align:top; width: 200px;",
         selectInput("var", "Variable", list.var, selected = NULL, multiple=FALSE,
@@ -32,8 +38,10 @@ ui <- fluidPage(
       #checkboxInput("persistence", "Include persistence", value=FALSE, width=NULL),
       #checkboxInput("climatology", "Include climatology", value=TRUE, width=NULL)
       
-     selectInput("type", "Score type", c('Continuous', 'Categorical'), width="150px"),
-      conditionalPanel(
+      #selectInput("score", "Skill score", list.skill.scores.short2, width="27%"),
+
+      selectInput("type", "Score type", c('Continuous', 'Categorical'), width="150px"),
+        conditionalPanel(
           condition = "input.type == 'Continuous'",
           div(style="display: inline-block;vertical-align:top; width: 150px;",
             selectInput("contscore", "Skill score", list.skill.scores.cont.short, width="100%"))),
@@ -43,7 +51,7 @@ ui <- fluidPage(
         div(style="display: inline-block;vertical-align:top; width: 150px;",
           selectInput("catscore", "Skill score", list.skill.scores.cat.short, width="100%")),
         div(style="display: inline-block;vertical-align:top; width: 150px;",
-          textInput("thr", "Threshold", value = ">0", width="100%"))
+          textInput("thr", "Threshold", value = ">20", width="100%"))
       )
     ),
     
